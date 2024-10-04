db00e0199d9023adb32fec86133e4befac16ac1b
mompei <ogunborjerry@gmail.com>
2024-10-04 14:36:27 +0100

    feat: added initilize user #[derive(Accounts)]

--- programs/nft-staking/src/state/stake_account.rs
+++ programs/nft-staking/src/state/stake_account.rs
@@ -1,9 +1,11 @@
 use anchor_lang::prelude::*;
 
 #[account]
+
+// per nft
 pub struct StakeAccount {
     pub owner: Pubkey,
-    pub mint: Pubkey,
+    pub mint: Pubkey, // mint of the nft that was staked
     pub last_update: i64,
     pub bump: u8,
 }
