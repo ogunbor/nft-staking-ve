a8fde5e979850b78405e9d857700b3584b6573f8
mompei <ogunborjerry@gmail.com>
2024-10-06 06:52:12 +0100

    add claim file

--- programs/nft-staking/src/instructions/mod.rs
+++ programs/nft-staking/src/instructions/mod.rs
@@ -1,10 +1,10 @@
-// pub mod claim;
+pub mod claim;
 pub mod initialize_config;
 pub mod initialize_user;
 pub mod stake;
 // pub mod unstake;
 
-// pub use claim::*;
+pub use claim::*;
 pub use initialize_config::*;
 pub use initialize_user::*;
 pub use stake::*;
